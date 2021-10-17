9d0f61c199a2321b42816dba2f8ddad334905b17
ehsansam69 <65367646+ehsansam69@users.noreply.github.com>
2021-10-16 21:17:19 -0400

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[20]:
 
 
 import numpy as np
@@ -9,6 +9,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import cv2 as cv
+import os
 
 from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.applications.densenet import DenseNet121
@@ -18,22 +20,29 @@ from keras import backend as K
 from keras.models import load_model
 
 
-# In[8]:
+# In[21]:
 
 
-pic = load_img("NOT_NORMAL\person1_bacteria_1.jpeg")
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
 
 
-# In[10]:
+# In[18]:
 
 
-pic_array = img_to_array(pic)
+not_normal = load_images_from_folder("NOT_NORMAL")
+normal = load_images_from_folder("NORMAL")
 
 
-# In[11]:
+# In[23]:
 
 
-pic_array
+plt.imshow(normal[0])
 
 
 # In[ ]:
